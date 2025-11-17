e4d264061b7f1a267a4214052f8b01e434121519
Lyioh <43170416+Lyioh@users.noreply.github.com>
2025-11-17 13:12:16 +0100

    Update Item.js

--- models/Item.js
+++ models/Item.js
@@ -1,13 +1,8 @@
-const mongoose = require('mongoose'); // Ensure mongoose is imported
-
-const itemSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    url: String,
-    imageUrl: String,
-    purchased: { type: Boolean, default: false }
-});
-
-const Item = mongoose.model('Item', itemSchema);
-
-module.exports = Item;
+[
+  {
+    "name": "test",
+    "price": "49€",
+    "image": "public/images/snowflake.png",
+    "status": "Disponible"
+  }
+]
